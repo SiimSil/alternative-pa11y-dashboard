2402f35f0f8af19dc610e4197c2323fd921ddfcc
SiimSil <siimuq@gmail.com>
2026-04-04 20:48:06 +0300

    feat: initial crawler

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -9,6 +9,8 @@ app.get('/', (req, res) => {
 })
 
 app.post('/scan', async (req, res) => {
+    var scanCount = 0;
+    var tasks = [];
     try {
         const url = req.body.url;
         const name = req.body.name
@@ -18,40 +20,50 @@ app.post('/scan', async (req, res) => {
             return res.status(400).json({ error: "URL is required" });
         }
 
-        const taskName = name || "Accessibility Scan";
-        const taskStandard = standard || "WCAG2AA";
+        var subpages = await crawl(url);
 
-        const response = await fetch('http://localhost:3000/tasks', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: taskName,
-            url: url,
-            standard: taskStandard
-        })
-        })
+        while (subpages.length > 0) {
+            let element = subpages.pop();
+            let taskName = name + ' ' + element.url || "Accessibility Scan";
+            let taskStandard = standard || "WCAG2AA";
 
-        if (!response.ok) {
-            throw new Error(`Pa11y task creation failed: ${response.status}`);
-        }
+            const response = await fetch('http://localhost:3000/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name: taskName,
+                    url: element.url,
+                    standard: taskStandard
+                })
+            })
+                
+            if (!response.ok) {
+                throw new Error(`Pa11y task creation failed: ${response.status}`);
+            }
 
-        const task = await response.json();
+            const task = await response.json();
 
-        const runResponse = await fetch(`http://localhost:3000/tasks/${task.id}/run`, {
-            method: 'POST'
-        })
+            const runResponse = await fetch(`http://localhost:3000/tasks/${task.id}/run`, {
+                method: 'POST'
+            })
 
-        if (!runResponse.ok) {
-            throw new Error(`Pa11y run failed: ${runResponse.status}`);
-        }
+            if (!runResponse.ok) {
+                throw new Error(`Pa11y run failed: ${runResponse.status}`);
+            }
 
+            scanCount++;
+            tasks.push({
+                url: element.url,
+                taskId: task.id
+            })
+        };
         res.json({
-            message: "Task created and started",
-            taskId: task.id
-        })
-        
+            message: "Tasks created and started",
+            pagesScanned: scanCount,
+            tasks: tasks
+        }) 
     } catch (e) {
         console.error(e);
 
@@ -60,8 +72,72 @@ app.post('/scan', async (req, res) => {
             msg: e.message
         });
     }
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+async function crawl(url) {
+    const visited = new Set();
+    const originUrl = new URL(url)
+    const depthLimit = 3;
+
+    const queue = [{url: url, depth: 0}];
+    const results = [];
+
+    const { chromium } = require('playwright');
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    while (queue.length>0) {
+        let link = queue.shift(); //take element
+
+        let linkUrl = new URL(link.url, originUrl);
+        let normUrl = linkUrl.hostname+linkUrl.pathname
+        visited.add(normUrl);
+
+        try {
+            var resultObj = null;
+            await page.goto(linkUrl.href);
+            await page.waitForLoadState('load'); //wait for DOM to load
+            var links = []
+
+            if (link.depth<depthLimit) {
+                links = (await page.$$eval('a', links => links.map(link => link.href)));
+                links.forEach(element => {
+                    try {
+                    let elUrl = new URL(element, originUrl)
+                    let normElUrl = elUrl.hostname+elUrl.pathname
+                    
+                    if (!visited.has(normElUrl) && //has been visited?
+                    !queue.some(queueEl => 
+                        {let queueElUrl = new URL(queueEl.url)
+                        let normQueueEl = queueElUrl.hostname + queueElUrl.pathname
+                        return normQueueEl == normElUrl}) && //is duplicate?
+                    originUrl.hostname == elUrl.hostname) //is external link?
+                        queue.push({url: elUrl.href, depth: link.depth+1})
+                    }
+                    catch (e) {
+                        console.error("Could not parse URL: "+e)
+                    }
+                });
+            }
+            let dom = await page.content();
+            
+            resultObj = {
+                url: link.url,
+                dom: dom,
+                links: links
+            }
+        }
+        catch (e) {
+            console.error(e)
+        }
+        if(resultObj)
+            results.push(resultObj)
+    }
+    await browser.close();
+    return results;
+}

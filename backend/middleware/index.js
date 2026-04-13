15224c06afca7bc0bc58238bb72ff5facfff03e8
SiimSil <siimuq@gmail.com>
2026-04-13 19:54:34 +0300

    feat: database integration + debug logging

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const { MongoClient, ObjectId } = require('mongodb');
 const app = express()
 const port = 4000
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'pa11y-middleware';
 
 app.use(express.json())
 
@@ -11,21 +19,57 @@ app.get('/', (req, res) => {
 app.post('/scan', async (req, res) => {
     var scanCount = 0;
     var tasks = [];
+    var requiresAuth = false;
+    var username = undefined;
+    var password = undefined;
+
+    await client.connect()
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+    const collection = db.collection('scans');
+    const id = new ObjectId();
     try {
         const url = req.body.url;
-        const name = req.body.name
-        const standard = req.body.standard
+        const name = req.body.name || "Accessibility Scan"
+
+        var config = {};
+        const allowedOptionals = ["ignore", "timeout", "wait", "hideElements", "headers", "actions"]
+        allowedOptionals.forEach(element => {
+            if (req.body[element]!=undefined && req.body[element]!=null)
+                config[element] = req.body[element]
+        });
+        const standard = req.body.standard || "WCAG2AA";
+        if(req.body["username"]!=undefined && 
+            req.body["username"]!=null &&
+            req.body["password"]!=undefined &&
+            req.body["password"]!=null) {
+            requiresAuth = true;
+            username = req.body["username"];
+            password = req.body["password"];
+        }
 
         if (!url) {
             return res.status(400).json({ error: "URL is required" });
         }
 
+        const insertResult = await collection.insertOne({
+            _id: id,
+            name: name, 
+            rootUrl: url,
+            standard: standard,
+            config: config,
+            requiresAuth: requiresAuth,
+            status: "running",
+            createdAt: new Date(),});
+        console.log('Inserted results into db =>', insertResult);
+
         var subpages = await crawl(url);
 
         while (subpages.length > 0) {
             let element = subpages.pop();
-            let taskName = name + ' ' + element.url || "Accessibility Scan";
-            let taskStandard = standard || "WCAG2AA";
+
+            console.log("Creating task: "+element.url)
 
             const response = await fetch('http://localhost:3000/tasks', {
                 method: 'POST',
@@ -33,9 +77,17 @@ app.post('/scan', async (req, res) => {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    name: taskName,
+                    name: name,
                     url: element.url,
-                    standard: taskStandard
+                    standard: standard,
+                    ignore: config.ignore,
+                    timeout: config.timeout,
+                    wait: config.wait,
+                    username: username,
+                    password: password,
+                    hideElements: config.hideElements,
+                    headers: config.headers,
+                    actions: config.actions
                 })
             })
                 
@@ -44,6 +96,8 @@ app.post('/scan', async (req, res) => {
             }
 
             const task = await response.json();
+
+            console.log("Running task: "+element.url)
 
             const runResponse = await fetch(`http://localhost:3000/tasks/${task.id}/run`, {
                 method: 'POST'
@@ -59,14 +113,17 @@ app.post('/scan', async (req, res) => {
                 taskId: task.id
             })
         };
+        let updateResult = await collection.updateOne({ _id: id }, { $set: { status: "completed", scanCount: scanCount } });
+        console.log('Updated status to completed =>', updateResult);
         res.json({
             message: "Tasks created and started",
-            pagesScanned: scanCount,
-            tasks: tasks
+            scanId: id,
+            scanCount: scanCount,
+            tasks
         }) 
     } catch (e) {
         console.error(e);
-
+        await collection.updateOne({ _id: id }, { $set: { status: "failed", scanCount: scanCount } });
         res.status(500).json({
             error: "Failed to start scan",
             msg: e.message
@@ -79,9 +136,11 @@ app.listen(port, () => {
 })
 
 async function crawl(url) {
+    console.log("Crawling started: "+url)
+
     const visited = new Set();
     const originUrl = new URL(url)
-    const depthLimit = 3;
+    const depthLimit = 0;
 
     const queue = [{url: url, depth: 0}];
     const results = [];
@@ -93,6 +152,9 @@ async function crawl(url) {
 
     while (queue.length>0) {
         let link = queue.shift(); //take element
+
+        console.log("Crawling: " + link.url)
+        console.log("Queue size: " + queue.length)
 
         let linkUrl = new URL(link.url, originUrl);
         let normUrl = linkUrl.hostname+linkUrl.pathname
@@ -124,11 +186,10 @@ async function crawl(url) {
                     }
                 });
             }
-            let dom = await page.content();
+            //let dom = await page.content();
             
             resultObj = {
                 url: link.url,
-                dom: dom,
                 links: links
             }
         }
@@ -138,6 +199,7 @@ async function crawl(url) {
         if(resultObj)
             results.push(resultObj)
     }
+    console.log("Crawling finished: "+url)
     await browser.close();
     return results;
 }

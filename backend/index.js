640edbb627d1cc3053897e7ced225f933cb455e7
SiimSil <siimuq@gmail.com>
2026-05-12 15:57:25 +0300

    fix: backend env

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+const path = require('path')
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+})
 const { MongoClient, ObjectId } = require('mongodb')
 const OpenAI = require('openai')
 const express = require('express')
